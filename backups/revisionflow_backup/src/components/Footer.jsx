9258b78e0b0a5b1d6023d75095dd30eefd407f9c
Oluwafemi Aisida <oluwafemiemmanuelaisida@gmail.com>
2026-03-18 23:34:04 +0000

    Remove broken submodules and ignore backups folder to fix Netlify deploy

--- backups/revisionflow_backup/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer style={{ padding: "1rem", textAlign: "center", backgroundColor: "#282c34", color: "white" }}>
-      &copy; 2026 RevisionFlow
-    </footer>
-  );
-}
