06867c8ffe0526d090c217d18103312c72d81c60
Oluwafemi Aisida <oluwafemiemmanuelaisida@gmail.com>
2026-04-13 13:10:06 +0100

    Refactor main.jsx to improve structure and theme loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,30 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import ErrorBoundary from './components/ErrorBoundary'
 import { TimerProvider } from './context/TimerContext'
+import { captureReferralFromUrl } from './utils/referrals'
+import { loadSavedTheme } from './data/themes'
 import './styles/globals.css'
+
+// Capture ?ref= from URL before React mounts (survives the auth redirect)
+captureReferralFromUrl()
+
+// Apply saved theme before first render (prevents flash of wrong colour)
+loadSavedTheme()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <ErrorBoundary>
+    <ErrorBoundary>
       <TimerProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </TimerProvider>
-      </ErrorBoundary>
-    </React.StrictMode>
+    </ErrorBoundary>
+  </React.StrictMode>
 )
 
 // PWA service worker registration
